0a58af48ea8ac331841a008f00fb1d212c41a269
Henri Guillot <henri.guihen01@gmail.com>
2026-05-09 17:39:51 -0400

    Deploy  stack

---
+++ load-testing/k6-loadtest.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 20,
+  duration: '2m',
+};
+
+export default function () {
+  http.get('http://localhost:8080');
+  sleep(1);
+}
